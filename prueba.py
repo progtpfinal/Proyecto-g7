6f69ef875b17f086152779d9eae5cd1efa84bb97
Ldituro <106823851+LDituro@users.noreply.github.com>
2026-06-11 02:33:42 +0000

    "lectura y guardado de la informacion del archivo"

---
+++ prueba.py
@@ -0,0 +1,22 @@
+def leer_archivo():
+    pacientes = {}
+
+    with open("drug_side_effects_10k.csv", "r") as archivo:
+
+        encabezados = archivo.readline().strip().split(",")
+
+        for linea in archivo:
+            valores = linea.strip().split(",")
+
+            id_paciente = valores[0]
+
+            datos = {}
+
+            for i in range(1, len(encabezados)):
+                datos[encabezados[i]] = valores[i]
+
+            pacientes[id_paciente] = datos
+
+    print(pacientes["PT-175721"])
+
+leer_archivo()
