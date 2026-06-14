3b199d80dd1a41dcba167810c04deef983d60017
Ldituro <106823851+LDituro@users.noreply.github.com>
2026-06-14 14:07:24 +0000

    "cambios de grafico de mapas, agregado de contador de casos fatales y severos todo en archivo pruebass"

--- pruebass.py
+++ pruebass.py
@@ -20,6 +20,35 @@ def leer_archivo():
             pacientes[id_paciente] = datos
     return pacientes
 
+def contar_pacientes_por_fecha(pacientes): #funcion refactorizada eliminacion del doble bucle
+
+    dicc_cant_fechas = {} 
+
+    for paciente in pacientes.values():
+        fecha = paciente["treatment_start_date"]
+        dicc_cant_fechas[fecha] = dicc_cant_fechas.get(fecha, 0) + 1
+
+    return dict(sorted(dicc_cant_fechas.items()))
+
+
+def mostar_grafico(fechas):
+
+    primer_mes = {}
+
+    for clave, valor in list(fechas.items())[::200]:#tomamos las 10 primeras fehcas usando las slicings y el cambio de tipo.
+        primer_mes[clave] = valor
+
+    fig, ax = plt.subplots()
+
+    ax.plot(primer_mes.keys(), 
+            primer_mes.values(), 
+            marker="o")  
+    ax.set_title("Inicio de Tratamientos")
+    ax.set_xlabel("Fechas")
+    ax.set_ylabel("cantidad")
+
+    return fig
+    
 def ubicacion_pacientes(pacientes,pais):
     cordenadas = []
     
@@ -32,18 +61,51 @@ def ubicacion_pacientes(pacientes,pais):
     
     return cordenadas
 
+def contar_casos_graves(pacientes, pais):
+
+    severos = 0
+    fatales = 0
+
+    for paciente in pacientes.values():
+
+        if paciente["country"] == pais:
+
+            if paciente["severity"] == "Severe":
+                severos += 1
+
+            elif paciente["outcome"] == "Fatal":
+                fatales += 1
+
+    return severos, fatales
+
 def main():
     pacientes = leer_archivo()
-    
+
+    fechas = contar_pacientes_por_fecha(pacientes)
+    fig = mostar_grafico(fechas)
 
     pais = st.selectbox("selecione un pais",
                         ["Australia","Canada","Germany","India",
                          "Pakistan","UK","USA"])
     
     cordenadas_pais = ubicacion_pacientes(pacientes,pais)
+    
+    severos , fatales = contar_casos_graves(pacientes, pais)
 
-    st.write("mostrando pacientes del pais ",pais)
-    st.map(cordenadas_pais[:20])
+    col1, col2 = st.columns(2)#considerar hacerlo por pestanias 
+    with col1:
 
+        st.subheader("Información de ",pais)
+
+        st.metric("Severos", severos)
+
+        st.metric("Fatales", fatales)
+
+    st.map(cordenadas_pais)
+
+    with col2:
+        st.subheader("Gráfico")
+        st.pyplot(fig)
+    
 if __name__=="__main__":
     main()
