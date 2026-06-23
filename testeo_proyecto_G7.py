e9505b14585d9cac06b6fa7e80a741e8b76f2178
Ldituro <106823851+LDituro@users.noreply.github.com>
2026-06-23 03:24:01 +0000

    "creacion de archivo prueba3.py, donde se realiza la ejecucion de la pregunta 4 "

--- testeo_proyecto_G7.py
+++ testeo_proyecto_G7.py
@@ -7,10 +7,34 @@ from prueba import *
 #graficos.
 
 def test_contar_casos_graves():
-    assert contar_casos_graves({1:{"country":"Pakistan","severity":"Mild","outcome":"Fatal"},2:{"country":"Pakistan","severity":"Mild","outcome":"Recovering"},3:{"country":"Pakistan","severity":"Mild","outcome":"Fatal"},4:{"country":"Pakistan","severity":"Moderate","outcome":"Fatal"},5:{"country":"Pakistan","severity":"Severe","outcome":"Fatal"}},"Pakistan") == (1,4)
-    assert contar_casos_graves({1:{"country":"USA","severity":"Mild","outcome":"Fatal"},2:{"country":"India","severity":"Mild","outcome":"Recovering"},3:{"country":"Germany","severity":"Mild","outcome":"Fatal"},4:{"country":"UK","severity":"Moderate","outcome":"Fatal"},5:{"country":"Canada","severity":"Severe","outcome":"Fatal"}},"Pakistan")==(0,0)
-    assert contar_casos_graves({1:{"country":"Pakistan","severity":"Mild","outcome":"Recovered"},2:{"country":"Pakistan","severity":"Mild","outcome":"Recovering"},3:{"country":"UK","severity":"Severe","outcome":"Fatal"},4:{"country":"Pakistan","severity":"Moderate","outcome":"Fatal"},5:{"country":"Pakistan","severity":"Severe","outcome":"Fatal"}},"UK")==(1,1)
-    assert contar_casos_graves({1:{"country":"Pakistan","severity":"Severe","outcome":"Recovering"},2:{"country":"USA","severity":"Mild","outcome":"Recovering"},3:{"country":"Pakistan","severity":"Mild","outcome":"Recovering"},4:{"country":"Pakistan","severity":"Moderate","outcome":"Hospitalized"},5:{"country":"Pakistan","severity":"Severe","outcome":"Recoverd"}},"Pakistan")== (2,0)
-    assert contar_casos_graves({1:{"country":"Canada","severity":"Moderate","outcome":"Fatal"},2:{"country":"Australia","severity":"Mild","outcome":"Recovering"},3:{"country":"Pakistan","severity":"Mild","outcome":"Fatal"},4:{"country":"Pakistan","severity":"Moderate","outcome":"Fatal"},5:{"country":"Pakistan","severity":"Severe","outcome":"Fatal"}},"Canada")==(0,1)
-    assert contar_casos_graves({1:{"country":"UK","severity":"Mild","outcome":"Fatal"},2:{"country":"Australia","severity":"Moderate","outcome":"Recovering"},3:{"country":"Pakistan","severity":"Mild","outcome":"Fatal"},4:{"country":"India","severity":"Moderate","outcome":"Fatal"},5:{"country":"Pakistan","severity":"Severe","outcome":"Fatal"}},"India")==(0,1)    
+    assert contar_casos_graves({1:{"country":"Pakistan","severity":"Mild","outcome":"Fatal"},
+                                2:{"country":"Pakistan","severity":"Mild","outcome":"Recovering"},
+                                3:{"country":"Pakistan","severity":"Mild","outcome":"Fatal"},
+                                4:{"country":"Pakistan","severity":"Moderate","outcome":"Fatal"},
+                                5:{"country":"Pakistan","severity":"Severe","outcome":"Fatal"}},"Pakistan") == (1,4)
+    assert contar_casos_graves({1:{"country":"USA","severity":"Mild","outcome":"Fatal"},
+                                2:{"country":"India","severity":"Mild","outcome":"Recovering"},
+                                3:{"country":"Germany","severity":"Mild","outcome":"Fatal"},
+                                4:{"country":"UK","severity":"Moderate","outcome":"Fatal"},
+                                5:{"country":"Canada","severity":"Severe","outcome":"Fatal"}},"Pakistan")==(0,0)
+    assert contar_casos_graves({1:{"country":"Pakistan","severity":"Mild","outcome":"Recovered"},
+                                2:{"country":"Pakistan","severity":"Mild","outcome":"Recovering"},
+                                3:{"country":"UK","severity":"Severe","outcome":"Fatal"},
+                                4:{"country":"Pakistan","severity":"Moderate","outcome":"Fatal"},
+                                5:{"country":"Pakistan","severity":"Severe","outcome":"Fatal"}},"UK")==(1,1)
+    assert contar_casos_graves({1:{"country":"Pakistan","severity":"Severe","outcome":"Recovering"},
+                                2:{"country":"USA","severity":"Mild","outcome":"Recovering"},
+                                3:{"country":"Pakistan","severity":"Mild","outcome":"Recovering"},
+                                4:{"country":"Pakistan","severity":"Moderate","outcome":"Hospitalized"},
+                                5:{"country":"Pakistan","severity":"Severe","outcome":"Recoverd"}},"Pakistan")== (2,0)
+    assert contar_casos_graves({1:{"country":"Canada","severity":"Moderate","outcome":"Fatal"},
+                                2:{"country":"Australia","severity":"Mild","outcome":"Recovering"},
+                                3:{"country":"Pakistan","severity":"Mild","outcome":"Fatal"},
+                                4:{"country":"Pakistan","severity":"Moderate","outcome":"Fatal"},
+                                5:{"country":"Pakistan","severity":"Severe","outcome":"Fatal"}},"Canada")==(0,1)
+    assert contar_casos_graves({1:{"country":"UK","severity":"Mild","outcome":"Fatal"},
+                                2:{"country":"Australia","severity":"Moderate","outcome":"Recovering"},
+                                3:{"country":"Pakistan","severity":"Mild","outcome":"Fatal"},
+                                4:{"country":"India","severity":"Moderate","outcome":"Fatal"},
+                                5:{"country":"Pakistan","severity":"Severe","outcome":"Fatal"}},"India")==(0,1)    
 
